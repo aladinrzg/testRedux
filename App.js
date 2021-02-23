1f014fd8385d130631e5b072c18a3b6da69e1657
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2021-02-23 18:54:36 +0100

    nothing

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
-import {Provider, useDispatch} from 'react-redux';
-import UserContainer from './components/userContainer';
-import User from './components/userContainer';
-import {store} from './redux/store';
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Provider, useDispatch } from "react-redux";
+import UserContainer from "./components/userContainer";
+import User from "./components/userContainer";
+import { store } from "./redux/store";
 
 function App(props) {
   return (
